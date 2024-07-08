9300fa10a0d6295da2632a5acd9f1ba5a0a65b0b
rezinsaleem <rezinsaleem1@gmail.com>
2024-07-09 02:40:51 +0530

    add user routes and pages 1st

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,20 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/user/Home";
+import SignIn from "./pages/user/SignIn";
+import SignUp from "./pages/user/SignUp";
+import Profile from "./pages/user/Profile";
 
 const App = () => {
   return (
-    <div className="text-red-800">App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
