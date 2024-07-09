2949c9ccec99f5e51c48148b95cf8f58e6c1ee0c
rezinsaleem <rezinsaleem1@gmail.com>
2024-07-09 16:52:21 +0530

    psql connected!

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
 import express from 'express';
+import  dotenv from 'dotenv';
+dotenv.config()
+import pkg from 'pg';
+const { Client } = pkg;
+
+const client  = new Client({
+  user:process.env.SQL_USER,
+  password:process.env.SQL_PASSWORD,
+  host:process.env.SQL_HOST,
+  port:process.env.SQL_PORT,
+  database:process.env.SQL_DB
+})
+
+client.connect()
+.then(()=> console.log('Database Connected Successfully'))
+.catch(e => console.log(e))
+.finally(()=> client.end())
 
 const app = express();
 
+const PORT = process.env.PORT;
 app.listen(3000, ()=>{
-  console.log("server is running on http://localhost:3000")
+  console.log(`server is running on http://localhost:${PORT}`)
 })
